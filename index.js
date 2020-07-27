69648a22e07d935e32c50524630fa195324c91cb
Gloria <loigloria2001@gmail.com>
2020-07-27 15:50:49 +0300

    create simple app

---
+++ index.js
@@ -0,0 +1,38 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const db = require('@paralect/node-mongo').connect("mongodb://db/docker");
+const service = db.createService('user');
+const loggerService = db.createService('logger');
+
+const router = Router();
+
+router.get('/me', async (ctx) => {
+  const user = await service.find();
+  ctx.status = 200;
+  ctx.body = user.results;
+})
+
+router.post('/logs', async (ctx) => {
+  if(!ctx.request.body.event){
+    ctx.body=JSON.stringify({message: "enter event plz"})
+  }
+  else {
+    await loggerService.create(ctx.request.body);
+    ctx.body=JSON.stringify({message: "completed"});
+  }
+});
+
+router.get('/logs', async (ctx) => {
+  const logs = await loggerService.find();
+  ctx.status = 200;
+  ctx.body = logs.results;
+});
+
+const app = new Koa();
+
+app.use(bodyParser());
+app.use(router.routes());
+
+app.listen(3030);
